f182fa7a7dec20bc6a049175ccfaa130803a38d5
1voodoo <pixel0101@mail.ru>
2022-02-19 14:47:33 +0300

    website-5

---
+++ Website-5/src/js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+console.log("Hello");
+let age = 41;
+let meLifeStyle = "freelance";
+let myName = "Фрилансер по жизни",
+    myAge = 36,
+    myMassage = "Живи. а работай в свободное время!";
+
+console.log(myAge);
